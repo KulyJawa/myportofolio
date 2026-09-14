eecbe61b5c35cbeb14f30818bde7378f47b6accc
KulyJawa <kulyjawa58@gmail.com>
2026-09-14 20:40:57 +0700

    Tugas 2 : menambahkan Education MVT dan refleksi README

--- main/models.py
+++ main/models.py
@@ -31,3 +31,16 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+
+# Model education
+class Education(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    start_year = models.CharField(max_length=4)
+    end_year = models.CharField(max_length=10)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.degree} - {self.institution}"
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Education
 
 
 def show_main(request):
@@ -21,3 +21,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+# Tambahan education
+def show_education(request):
+    context = {
+        "name": "Emil Ananta Kautsar",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
